67c81c2b61317cfac33a14a2a2cc0fecdc614c52
radhikaagarwal123 <141233354+radhikaagarwal123@users.noreply.github.com>
2023-08-08 18:22:29 +0530

    Add files via upload

---
+++ circle.java/src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+class circle {
+    public static void main(String[] args) {
+        double radius=5,pi=3.14;
+        double c;
+        c=pi*radius*radius;
+
+        System.out.println(c);
+
+    }
+}
